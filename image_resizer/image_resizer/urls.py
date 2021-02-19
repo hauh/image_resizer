d9b64953e1a3d3bd493354f54ef75e635d5e8e5f
hauh <hauh@ya.ru>
2021-02-19 15:23:33 +0300

    Deploy

--- image_resizer/image_resizer/urls.py
+++ image_resizer/image_resizer/urls.py
@@ -1,15 +1,18 @@
 """URL Configuration."""
 
 from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path
 from django.urls.conf import include
-from image_resizer_app.views import ListImages, UploadImage, ResizeImage
+from django.views.generic.base import RedirectView
+from django.views.static import serve
+from image_resizer_app.views import ListImages, ResizeImage, UploadImage
 
 urlpatterns = [
+	path('', RedirectView.as_view(url='image_resizer/')),
 	path('image_resizer/', include([
 		path('', ListImages.as_view(), name='image-list'),
 		path('image/', UploadImage.as_view(), name='image-create'),
 		path('image/<pk>/', ResizeImage.as_view(), name='image-update')
-	]))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+	])),
+	path('images/<path:path>', serve, {'document_root': settings.MEDIA_ROOT})
+]
